1f067fc29997b7b6edb9cff1c70b9e54f1a7e114
Weiliang Li <to.be.impressive@gmail.com>
2016-02-25 19:46:41 +0800

    Refine the server code

--- ServerTcpListener.java
+++ ServerTcpListener.java
@@ -2,9 +2,7 @@ import java.net.*;
 import java.io.*;
 
 public class ServerTcpListener {
-
     public static void main(String[] args) {
-
         try {
             final ServerSocket server = new ServerSocket();
             server.bind(new InetSocketAddress("0.0.0.0", 4242));
@@ -15,50 +13,43 @@ public class ServerTcpListener {
                     Socket socket = server.accept();
                     System.out.println("Success");
                     receiveFile(socket);
-
                 } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
-        }
-
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void run() {
-    }
-
     public static void receiveFile(Socket socket) {
-
         byte[] inputByte = null;
         int length = 0;
         DataInputStream dis = null;
         FileOutputStream fos = null;
-       
-            try {
+        try {
+            dis = new DataInputStream(socket.getInputStream());
+            fos = new FileOutputStream(new File("C:\\Users\\Will\\Desktop\\receivedFile.PNG"));
+            inputByte = new byte[1024 * 4];
+            System.out.println("Receiving...");
+            while ((length = dis.read(inputByte, 0, inputByte.length)) > 0) {
+                fos.write(inputByte, 0, length);
+                fos.flush();
+            }
+            System.out.println("Finish");
 
-                dis = new DataInputStream(socket.getInputStream());
-                fos = new FileOutputStream(new File("C:\\Users\\Will\\Desktop\\aaabbb.PNG"));
-                inputByte = new byte[1024 * 4];
-                System.out.println("Receiving...");
-                while ((length = dis.read(inputByte, 0, inputByte.length)) > 0) {
-                    fos.write(inputByte, 0, length);
-                    fos.flush();
-                }
-                System.out.println("Finish");
+            //return the result
+            String res = "Finish";
+            OutputStream out = socket.getOutputStream();
+            out.write(res.getBytes());
 
-
-                //return the result
-                String res = "Finish";
-                OutputStream out = socket.getOutputStream();
-                out.write(res.getBytes());
-                //closing resources
-                socket.close();
-                dis.close();
-                fos.close();
-                out.close();
-            } catch (Exception e){e.printStackTrace();}
-        
+            //closing resources
+            socket.close();
+            dis.close();
+            fos.close();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
