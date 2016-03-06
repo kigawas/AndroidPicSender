109907c9ccc2c976ef5086ce8824070aff54dcd0
Weiliang Li <to.be.impressive@gmail.com>
2016-03-06 18:09:13 +0800

    Rename the server code file

--- Server.java
+++ Server.java
@@ -1,7 +1,7 @@
 import java.net.*;
 import java.io.*;
 
-public class ServerTcpListener {
+public class Server {
     public static void main(String[] args) {
         try {
             final ServerSocket server = new ServerSocket();
